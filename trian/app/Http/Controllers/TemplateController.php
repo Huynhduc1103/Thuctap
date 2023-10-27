f9e0bac7ed5fcbcbb7474d46bdaedb8fd17a2dd2
Phùng Nhật Linh <119924582+LinhPN05@users.noreply.github.com>
2023-10-27 16:50:54 +0700

    Cập nhật lại Temp theo ý Đức
    - Type không được nhập
    - if data >= 1 then Type == USER
    - if data >= 2 then Type == GROUP
    - Timer lấy ngày giờ hiện tại

--- trian/app/Http/Controllers/TemplateController.php
+++ trian/app/Http/Controllers/TemplateController.php
@@ -13,31 +13,26 @@ class TemplateController extends Controller
         return response()->json($template);
     }
 
+
     public function create(Request $request) {
-        $timer = $request->input('timer');
-        $type = $request->input('type');
+        
+        $timer = date('Y-m-d H:i:s'); // Lấy ngày giờ hiện tại
         $data = $request->input('data');
         $message_id = $request->input('message_id');
         $event_id = $request->input('event_id');
-    
-        if ($type === 'GROUP') {
-            // Kiểm tra xem dữ liệu có ít nhất 2 dữ liệu trở lên (sử dụng dấu phẩy để phân tách)
-            $dataArray = explode(',', $data);
-            if (count($dataArray) < 2) {
-                return response()->json(['error' => 'Dữ liệu phải chứa ít nhất 2 dữ liệu nếu loại là "GROUP".'], 400);
-            }
-        } elseif ($type === 'USER') {
-            // Kiểm tra xem dữ liệu chỉ chứa một giá trị
-            $dataArray = explode(',', $data);
-            if (count($dataArray) != 1) {
-                return response()->json(['error' => 'Dữ liệu chỉ được phép chứa một giá trị nếu loại là "USER".'], 400);
-            }
+        
+        // Tự động xác định giá trị cho trường "type" dựa trên dữ liệu
+        $dataArray = explode(',', $data);
+        if (count($dataArray) == 1) {
+            $type = 'USER';
+        } elseif (count($dataArray) >= 2) {
+            $type = 'GROUP';
         } else {
-            return response()->json(['error' => 'Type không hợp lệ. Type phải là "GROUP" hoặc "USER".'], 400);
+            return response()->json(['error' => 'Dữ liệu không hợp lệ.'], 400);
         }
     
-        // Sau khi kiểm tra, tạo sự kiện nếu không có lỗi
-        $event = Template::create([
+        // Tiến hành thêm dữ liệu
+        $template = Template::create([
             'timer' => $timer,
             'type' => $type,
             'data' => $data,
@@ -45,53 +40,50 @@ class TemplateController extends Controller
             'event_id' => $event_id,
         ]);
     
-        return response()->json($event);
+        return response()->json($template);
     }
     
-    
 
+    public function update($id, Request $request) {
+        $template = Template::find($id);
+    
+        if (!$template) {
+            return response()->json(['error' => 'Sự kiện không tồn tại.'], 404);
+        }
+    
+        $timer = date('Y-m-d H:i:s'); // Lấy ngày giờ hiện tại
+        $data = $request->input('data');
+        $message_id = $request->input('message_id');
+        $event_id = $request->input('event_id');
+    
+        // Tự động xác định giá trị cho trường "type" dựa trên dữ liệu
+        $dataArray = explode(',', $data);
+        if (count($dataArray) == 1) {
+            $type = 'USER';
+        } elseif (count($dataArray) >= 2) {
+            $type = 'GROUP';
+        } else {
+            return response()->json(['error' => 'Dữ liệu không hợp lệ.'], 400);
+        }
+    
+        // Cập nhật dữ liệu cho sự kiện có ID tương ứng
+        $template->timer = $timer;
+        $template->type = $type;
+        $template->data = $data;
+        $template->message_id = $message_id;
+        $template->event_id = $event_id;
+        $template->save();
+    
+        return response()->json($template);
+    }
+    
+       
     public function findId(Request $request) {
         $id = $request->input('id');
         $template = Template::find($id);
         return response()->json($template);
     }
 
-    public function update($id, Request $request) {
-        $template = Template::find($id);
-
-        $timer = $request->input('timer');
-        $type = $request->input('type');
-        $data = $request->input('data');
-        $message_id = $request->input('message_id');
-        $event_id = $request->input('event_id');
-    
-        if ($type === 'GROUP') {
-            // Kiểm tra xem dữ liệu có ít nhất 2 dữ liệu trở lên (sử dụng dấu phẩy để phân tách)
-            $dataArray = explode(',', $data);
-            if (count($dataArray) < 2) {
-                return response()->json(['error' => 'Dữ liệu phải chứa ít nhất 2 dữ liệu nếu loại là "GROUP".'], 400);
-            }
-        } elseif ($type === 'USER') {
-            // Kiểm tra xem dữ liệu chỉ chứa một giá trị
-            $dataArray = explode(',', $data);
-            if (count($dataArray) != 1) {
-                return response()->json(['error' => 'Dữ liệu chỉ được phép chứa một giá trị nếu loại là "USER".'], 400);
-            }
-        } else {
-            return response()->json(['error' => 'Type không hợp lệ. Type phải là "GROUP" hoặc "USER".'], 400);
-        }
-    
-        // Sau khi kiểm tra, tạo sự kiện nếu không có lỗi
-        $event = Template::create([
-            'timer' => $timer,
-            'type' => $type,
-            'data' => $data,
-            'message_id' => $message_id,
-            'event_id' => $event_id,
-        ]);
-    
-        return response()->json($event);
-    }
 
     public function delete($id) {
         $template = Template::find($id);
@@ -99,7 +91,7 @@ class TemplateController extends Controller
             // Xóa 
             $template->delete();
             // Xóa thành công
-            return response()->json(['message' => 'template ' . $id . ' đã được xóa thành công']);
+            return response()->json(['message' => 'Template ' . $id . ' đã được xóa thành công']);
         } catch (\Exception $e) {
             // Xử lý lỗi nếu có
             return response()->json(['message' => 'Lỗi xóa template: ' . $e->getMessage()], 500);
